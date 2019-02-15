709d5d27235946bf68f8425e0854cfb3eb59738b
Vignesh Sankara Narayanan <vignesh.sankaranarayanan87@gmail.com>
2019-02-15 07:55:03 -0600

    routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,28 @@ import {CodingComponent} from "./coding/coding.component";
 import {ExperienceComponent} from "./experience/experience.component";
 import {PublishedComponent} from "./published/published.component";
 
+import {  
+  PlatformLocation,  
+  Location,  
+  LocationStrategy,  
+  HashLocationStrategy,  
+  PathLocationStrategy,  
+  APP_BASE_HREF
+} from '@angular/common'; 
+
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'coding', component: CodingComponent },
   { path: 'experience', component: ExperienceComponent },
   { path: 'projects', component: PublishedComponent },
-  {path : '', component : HomeComponent}
+  {path : '**', component : HomeComponent}
 ];
 
-export const routing = RouterModule.forRoot(routes);
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class routing { }
 
